daf0ecee14d92258d6cfa15d7809962a35c10cf0
Amanda Otero <mandystar018@gmail.com>
2020-08-17 15:11:29 -0700

    This commit will add sort method and .toUpperCase method to our project
    Co-Authored-by: Jillian Gibson  <jillian.l.gibson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
   $("form#grocery").submit(function(event){
-    event.preventDefaul();
+    event.preventDefault();
     const item1 = $("input#item1").val();
     const item2 = $("input#item2").val();
     const item3 = $("input#item3").val();
     const item4 = $("input#item4").val();
     const item5 = $("input#item5").val();
     const groceryItems = [item1, item2, item3, item4, item5];
+    groceryItems.sort
 
-    // $("#form").submit();
 
     groceryItems.forEach(function(element){
-      $(element).text(input);
+      $("#grocery-list").append("<li>" + element.toUpperCase() + "</li>");
     });
     $("form#grocery").hide();
-    $(".groceryList").show();
+    $(".output").show();
   });
 });
